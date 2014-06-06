a830e52809a4f3e377ee83f3eebdcbc01df47717
vegerjiang <jackhenizaiyiqi@qq.com>
2014-06-07 02:48:00 +0800

    fasdf

--- DIYFilter/DIYFilter/config.js
+++ DIYFilter/DIYFilter/config.js
@@ -3,15 +3,15 @@
  
     "move":{
         "filterName":"move",
-        "filterType":"base",
-        "configPath":"base/move/move.config",
-        "fshPath":"base/move/move.fsh"
+        "filterType":"VGBase",
+        "configPath":"VGBase/move/move.config",
+        "fshPath":"VGBase/move/move.fsh"
     },
     "pinch":{
         "filterName":"pinch",
-        "filterType":"base",
-        "configPath":"base/pinch/pinch.config",
-        "fshPath":"base/pinch/pinch.fsh"
+        "filterType":"VGBase",
+        "configPath":"VGBase/pinch/pinch.config",
+        "fshPath":"VGBase/pinch/pinch.fsh"
     },
  
  
@@ -19,18 +19,18 @@
  
     "test1":{
         "filterName":"test1",
-        "filterType":"combination",
-        "configPath":"combination/test1/test1.config"
+        "filterType":"Combination",
+        "configPath":"Combination/test1/test1.config"
     },
     "test2":{
         "filterName":"test2",
-        "filterType":"combination",
-        "configPath":"combination/test2/test2.config"
+        "filterType":"Combination",
+        "configPath":"Combination/test2/test2.config"
     },
     "test3":{
         "filterName":"test3",
-        "filterType":"combination",
-        "configPath":"combination/test3/test3.config"
+        "filterType":"Combination",
+        "configPath":"Combination/test3/test3.config"
     }
  
  

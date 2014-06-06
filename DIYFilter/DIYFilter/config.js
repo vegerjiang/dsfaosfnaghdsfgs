f762fb93546020b6ca58008d2ac7a1df38895c39
vegerjiang <jackhenizaiyiqi@qq.com>
2014-06-07 01:38:05 +0800

    khklhlk

---
+++ DIYFilter/DIYFilter/config.js
@@ -0,0 +1,37 @@
+[
+ 
+ 
+    {
+        "filterName":"move",
+        "filterType":"base",
+        "configPath":"base/move/move.config",
+        "fshPath":"base/move/move.fsh"
+    },
+    {
+        "filterName":"pinch",
+        "filterType":"base",
+        "configPath":"base/pinch/pinch.config",
+        "fshPath":"base/pinch/pinch.fsh"
+    },
+ 
+ 
+ 
+ 
+    {
+        "filterName":"test1",
+        "filterType":"combination",
+        "configPath":"combination/test1/test1.config"
+    },
+    {
+        "filterName":"test2",
+        "filterType":"combination",
+        "configPath":"combination/test2/test2.config"
+    },
+    {
+        "filterName":"test3",
+        "filterType":"combination",
+        "configPath":"combination/test3/test3.config"
+    }
+ 
+ 
+]

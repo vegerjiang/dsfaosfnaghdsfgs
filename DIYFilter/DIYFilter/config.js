432b6dc5c4da088a088d6ca57f8175f18307b2aa
vegerjiang <jackhenizaiyiqi@qq.com>
2014-06-07 01:52:40 +0800

    fsdafasd

--- DIYFilter/DIYFilter/config.js
+++ DIYFilter/DIYFilter/config.js
@@ -1,13 +1,13 @@
-[
+{
  
  
-    {
+    "move":{
         "filterName":"move",
         "filterType":"base",
         "configPath":"base/move/move.config",
         "fshPath":"base/move/move.fsh"
     },
-    {
+    "pinch":{
         "filterName":"pinch",
         "filterType":"base",
         "configPath":"base/pinch/pinch.config",
@@ -17,21 +17,21 @@
  
  
  
-    {
+    "test1":{
         "filterName":"test1",
         "filterType":"combination",
         "configPath":"combination/test1/test1.config"
     },
-    {
+    "test2":{
         "filterName":"test2",
         "filterType":"combination",
         "configPath":"combination/test2/test2.config"
     },
-    {
+    "test3":{
         "filterName":"test3",
         "filterType":"combination",
         "configPath":"combination/test3/test3.config"
     }
  
  
-]
+}
